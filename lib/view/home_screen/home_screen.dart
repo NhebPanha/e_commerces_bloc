c57087fbdf246c3fc485fb3a905aa9bb59d22ef5
panhanheb <panhanheb78@gmail.com>
2025-09-14 11:50:35 +0700

    dd

---
+++ lib/view/home_screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:e_com_bloc/view/home_screen/widget/custom_search_widget.dart';
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CustomAppbar(title: 'Locations', icons: Icons.location_on, descriptions: "Cambodia", iconNotification: Icons.notification_important_sharp,),
+          CustomSearchWidget(title: 'Search', icons: Icons.search,),
+        ]
+      )
+    );
+  }
+}
